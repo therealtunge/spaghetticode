860b8ac3b5edeeb49d20b6b53da82147547930ab
therealtunge <behradsharifi8@gmail.com>
2026-02-01 17:32:55 +0330

    FINALLY IT WORKS

--- vm.c
+++ vm.c
@@ -15,7 +15,9 @@ static inline char *parse_pointer(void *ram, unsigned int ptr) {
 static inline int8_t parse_i8(void *ram, unsigned int ptr) {
 	return *((int8_t*) (ram + ptr));
 }
-
+static inline uint8_t parse_u8(void *ram, unsigned int ptr) {
+	return *((uint8_t*) (ram + ptr));
+}
 static inline uint16_t parse_u16(void *ram, unsigned int ptr) {
 	return *((uint16_t*) (ram + ptr));
 }
@@ -32,14 +34,17 @@ static inline void write_u32(void *ram, unsigned int ptr, uint32_t value) {
 
 
 void push_u32(VMstate_t *vm, uint32_t value) {
+	vm->callsp += 1;
+	//printf("%d AAAAA\n", vm->callsp);
 
 	if (vm->callstacksize <= vm->callsp) {
 		vm->callstacksize += 4;
 		vm->callstack = realloc(vm->callstack, vm->callstacksize);
 	}
-	write_u32(vm->callstack, vm->callsp, value);
+	vm->callstack[vm->callsp] = value;
 	
-	vm->callsp += 4;
+	
+	
 //	printf("ABDASDADBZASVDSDBXDFCZFVDGBGMHGVFNH %d %d %d\n", vm->callstacksize, vm->callsp, value);
 }
 void push_regu32(VMstate_t *vm, uint32_t value) {
@@ -56,9 +61,8 @@ uint32_t pop_regu32(VMstate_t *vm) {
 	return parse_u32(vm->regstack, vm->regsp);
 }
 uint32_t pop_u32(VMstate_t *vm) {
-	vm->callsp -= 4;
-	//printf("AAA %d %d\n", vm->callstacksize, vm->callsp);
-	return parse_u32(vm->callstack, vm->callsp);
+	//vm->callsp -= 1;
+	return vm->callstack[vm->callsp--];
 }
 
 bool is_address_method(VMstate_t *vm, unsigned int addr) {
@@ -100,7 +104,7 @@ int processInstruction(VMstate_t *vm) {
 	
 	switch (vm->ram[vm->pc]) {
 		case 0x00: {
-			printf("AAAAAA\n");
+		//	printf("AAAAAA\n");
 			vm->pc += 2;
 			return 0;
 		}
@@ -116,7 +120,7 @@ int processInstruction(VMstate_t *vm) {
 				printf("main() returned exit code %d\n", vm->result);
 				exit(vm->result);
 			}
-			for (int i = -1; i < vm->cur_method_locals; i++) {
+			for (int i = 0; i < vm->cur_method_locals; i++) {
 				pop_regu32(vm);
 			}
 			vm->pc = pop_u32(vm);
@@ -124,7 +128,7 @@ int processInstruction(VMstate_t *vm) {
 			
 			printf("RETURN %d\n", vm->result);
 			
-			printf("ABBBB %d\n", vm->pc);
+			//printf("ABBBB %d\n", vm->pc);
 			return 0;
 		}
 		case 0x12: {
@@ -146,13 +150,18 @@ int processInstruction(VMstate_t *vm) {
 			return 0;
 		}
 		case 0x71: {
-			printf("AaA %d, %d\n", vm->methods[parse_u16(vm->ram, vm->pc + 2)].address, vm->pc + 4);
+			//printf("AaA %d, %d\n", vm->methods[parse_u16(vm->ram, vm->pc + 2)].address, vm->pc + 4);
 			push_u32(vm, vm->pc+4);
 			vm->pc = vm->methods[parse_u16(vm->ram, vm->pc + 2)].address;
 			return 0;
 		}
 		case 0xd0: {
 			write_register(vm->ram[vm->pc + 1] & 0xf, read_register((vm->ram[vm->pc + 1] & 0xf0) >> 4) + parse_u16(vm->ram, vm->pc + 2));
+			vm->pc += 4;
+			return 0;
+		}
+		case 0xd8: {
+			write_register(vm->ram[vm->pc + 1] & 0xf, read_register((vm->ram[vm->pc + 1] & 0xf0) >> 4) + parse_u8(vm->ram, vm->pc + 3));
 			vm->pc += 4;
 			return 0;
 		}
